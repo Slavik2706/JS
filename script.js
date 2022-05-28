d0a156a68c88dd9a6489237df8575f89832cbdca
Slavik2706 <grinko-s@mail.ru>
2022-05-28 15:50:57 +0300

    Выполнил ДЗ №2

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "JS";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 5;
+let rollback = 10;
+let fullPrice = 10000;
+let adaptive = true;
 
-alert('Я изучаю JS');
-console.log('Я изучаю JS');
+console.log("Тип данных переменной title: " + typeof title);
+console.log("Тип данных переменной fullPrice: " + typeof fullPrice);
+console.log("Тип данных переменной adaptive: " + typeof adaptive);
+
+console.log("Длина строки из переменной screens: " + screens.length);
+console.log("Стоимость верстки экранов " + screenPrice + " рублей");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей");
+
+console.log(screens.toLowerCase().split(", "));
+console.log("Процент отката посреднику за работу " + fullPrice * (rollback / 100) + " рублей");
