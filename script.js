42cadb3f91333162a4e1d556801ed473376febcc
Slavik2706 <grinko-s@mail.ru>
2022-06-02 23:07:03 +0300

    Внесены правки №2

--- script.js
+++ script.js
@@ -16,14 +16,15 @@ const isNumber = function (num) {
 
 const asking = function () {
    title = prompt("Как называется ваш проект?", "rrh");
-   screens = prompt("Какие типы экранов нужно разработать?", "ghh");
-   screenPrice = prompt("Сколько будет стоить данная работа?");
-   screenPrice = screenPrice.replace(/\s/g, '');
+   screens = prompt("Какие типы экранов нужно разработать?");
 
-   while (!isNumber(screenPrice)) {
+   do {
       screenPrice = prompt("Сколько будет стоить данная работа?");
-      screenPrice = screenPrice.replace(/\s/g, '');
-   }
+      if (screenPrice !== null) {
+         screenPrice = screenPrice.replace(/\s/g, '');
+      }
+   } while (!isNumber(screenPrice))
+
    screenPrice = Number(screenPrice);
    adaptive = confirm("Нужен ли адаптив на сайте?");
 }
@@ -33,16 +34,19 @@ const getAllServicePrices = function () {
    let price;
    for (let i = 0; i < 2; i++) {
       if (i === 0) {
-         service1 = prompt("Какой дополнительный тип услуги нужен?", "hrh");
+         service1 = prompt("Какой дополнительный тип услуги нужен?");
       } else if (i === 1) {
-         service2 = prompt("Какой дополнительный тип услуги нужен?", "hrhr");
+         service2 = prompt("Какой дополнительный тип услуги нужен?");
       }
       price = prompt("Сколько это будет стоить?")
-      price = price.replace(/\s/g, '')
-
+      if (price !== null) {
+         price = price.replace(/\s/g, '')
+      }
       while (!isNumber(price)) {
          price = prompt("Сколько это будет стоить?")
-         price = price.replace(/\s/g, '')
+         if (price !== null) {
+            price = price.replace(/\s/g, '')
+         }
       }
       sum += Number(price)
    }
